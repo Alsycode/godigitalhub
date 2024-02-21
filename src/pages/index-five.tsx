d4b49c2a6d2ccb442023a21c5484bb6cdfad7b20
Alsycode <120390865+Alsycode@users.noreply.github.com>
2024-02-21 16:21:52 +0530

    home-five

--- src/pages/index-five.tsx
+++ src/pages/index-five.tsx
@@ -2,12 +2,13 @@ import React from "react";
 import Layout from "@/components/layout/Layout";
 import HomeFiveBanner from "@/components/layout/banner/HomeFiveBanner";
 import HomeTestimonial from "@/components/containers/home/HomeTestimonial";
+const testimonialsData = [1,2]; 
 
 const HomeFive = () => {
   return (
     <Layout header={5} footer={4} video={true}>
       <HomeFiveBanner />
-      <HomeTestimonial />
+      <HomeTestimonial testimonials={testimonialsData}/>
     </Layout>
   );
 };
